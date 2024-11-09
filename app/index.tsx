0415f6caabf0729f5a0f13d384eb6c54171936fb
Reza Buzarjemehri <dev.reza21@gmail.com>
2024-11-09 21:19:25 +0330

    init project

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+// import { Slot } from "expo-router";
+
+// export default function Layout() {
+//   return <Slot />;
+// }
+
+import { Text } from "react-native";
+
+export default function Page() {
+  return <Text>Top-level page</Text>;
+}
